4c4c59827abac02272fd8baf89d24c089e1d7add
rock90386 <44420116+rock90386@users.noreply.github.com>
2019-03-06 15:34:06 +0800

    homework

--- routes.js
+++ routes.js
@@ -8,31 +8,33 @@ var appRouter = function (app) {
     res.status(200).send({ message: 'Welcome to our restful API' });
   });
 
-  app.get("/user", function (req, res) {
+  app.get("/student", function (req, res) {
     var data = ({
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),
       username: faker.internet.userName(),
-      email: faker.internet.email()
+      email: faker.internet.email(),
+      id: faker.internet.id()
     });
     res.status(200).send(data);
   });
 
- app.get("/users/:num", function (req, res) {
-   var users = [];
+ app.get("/students/:num", function (req, res) {
+   var students = [];
    var num = req.params.num;
 
    if (isFinite(num) && num  > 0 ) {
      for (i = 0; i <= num-1; i++) {
-       users.push({
+       students.push({
            firstName: faker.name.firstName(),
            lastName: faker.name.lastName(),
            username: faker.internet.userName(),
-           email: faker.internet.email()
+           email: faker.internet.email(),
+           id: faker.random.uuid()
         });
      }
 
-     res.status(200).send(users);
+     res.status(200).send(students);
     
    } else {
      res.status(400).send({ message: 'invalid number supplied' });
